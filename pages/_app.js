493b0f2b9070cc87bb9de56ccf3c19b5e92eaf74
Petar Brkić <brkicpetar@pm.me>
2026-04-10 16:50:39 +0200

    bugfix: blank page still

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "../styles/globals.css";
 import Head from "next/head";
 export default function App({ Component, pageProps }) {
-  return 
+  return (
   <>
     <Head>
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <Component {...pageProps} />
-  </>;
+  </>
+  );
 }
